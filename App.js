3fcf4f015a452709ca0a5c55fdf67b4f4d0fe20d
Karan Kumar <karan.kumar@bainslabs.com>
2019-04-10 18:38:15 +0530

    redux setup

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
+import React, { Component } from 'react'
 import { createStackNavigator, createAppContainer } from "react-navigation";
+import { initStore } from './redux/store';
+import { Provider } from 'react-redux';
 import Camera from './components/Camera';
 import Home from './components/Home'
 
+const store = initStore();
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Home },
@@ -16,7 +20,15 @@ const MainNavigator = createStackNavigator({
     }
   });
 
-const App = createAppContainer(MainNavigator);
+const Navigation = createAppContainer(MainNavigator);
 
-
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Navigation />
+      </Provider>
+    );
+  }
+}
 export default App;
